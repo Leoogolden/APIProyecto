e1ba8d76020a4838897d89bfc155c0381ba7abab
Android Studio <devandroid@ort>
2019-09-06 11:35:59 -0300

    ahi va

---
+++ ApiEjemplo/Data/ActividadData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ApiEjemplo.Data
+{
+    public class ActividadData
+    {
+
+        private static string conexionString = "Server=10.128.8.16;Database=VoyBD;User ID=usr_voybd ;pwd=proyectoort2019+ ;";
+        private static SqlConnection Conectar()
+        {
+            SqlConnection a = new SqlConnection(conexionString);
+            a.Open();
+            return a;
+        }
+        private static void Desconectar(SqlConnection conector)
+        {
+            conector.Close();
+        }
+        public static int insertarActividad(string nombre, string descripcion, int edadMin, int edadMax, int limPer, string calle, int direccion, DateTime fecha)
+        {
+            SqlConnection Conexion = Conectar();
+            SqlCommand consulta = Conexion.CreateCommand();
+            consulta.CommandText = "AgregarActiv";
+            consulta.CommandType = System.Data.CommandType.StoredProcedure;
+            consulta.Parameters.AddWithValue("@Fecha", fecha);
+            consulta.Parameters.AddWithValue("@EdadMin", edadMin);
+            consulta.Parameters.AddWithValue("@EdadMax",edadMax );
+            consulta.Parameters.AddWithValue("@LimPer",limPer );
+            consulta.Parameters.AddWithValue("@Calle",calle );
+            consulta.Parameters.AddWithValue("@Dir",direccion );
+            consulta.Parameters.AddWithValue("@Nom",nombre );
+            consulta.Parameters.AddWithValue("@Desc",descripcion );
+            int regsAfectados = Convert.ToInt32(consulta.ExecuteScalar());
+            Desconectar(Conexion);
+            return regsAfectados;
+        }
+
+    }
+}
---
+++ ApiEjemplo/Models/Actividad.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiEjemplo.Models
+{
+    public class Actividad
+    {
+        private int idActiv;
+
+        public int GetIdActiv()
+        {
+            return idActiv;
+        }
+
+        public void SetIdActiv(int value)
+        {
+            idActiv = value;
+        }
+
+        private String nombre;
+
+        public String GetNombre()
+        {
+            return nombre;
+        }
+
+        public void SetNombre(String value)
+        {
+            nombre = value;
+        }
+
+        private String descripcion;
+
+        public String GetDescripcion()
+        {
+            return descripcion;
+        }
+
+        public void SetDescripcion(String value)
+        {
+            descripcion = value;
+        }
+
+        private int edadMin;
+
+        public int GetEdadMin()
+        {
+            return edadMin;
+        }
+
+        public void SetEdadMin(int value)
+        {
+            edadMin = value;
+        }
+
+        private int edadMax;
+
+        public int GetEdadMax()
+        {
+            return edadMax;
+        }
+
+        public void SetEdadMax(int value)
+        {
+            edadMax = value;
+        }
+
+        private int limPer;
+
+        public int GetLimPer()
+        {
+            return limPer;
+        }
+
+        public void SetLimPer(int value)
+        {
+            limPer = value;
+        }
+
+        private string calle;
+
+        public string GetCalle()
+        {
+            return calle;
+        }
+
+        public void SetCalle(string value)
+        {
+            calle = value;
+        }
+
+        private int direccion;
+
+        public int GetDireccion()
+        {
+            return direccion;
+        }
+
+        public void SetDireccion(int value)
+        {
+            direccion = value;
+        }
+
+        private DateTime fecha;
+
+        public DateTime GetFecha()
+        {
+            return fecha;
+        }
+
+        public void SetFecha(DateTime value)
+        {
+            fecha = value;
+        }
+
+        public Actividad(int idActiv, string nombre, string descripcion, int edadMin, int edadMax, int limPer, string calle, int direccion, DateTime fecha)
+        {
+            this.idActiv = idActiv;
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+            this.edadMin = edadMin;
+            this.edadMax = edadMax;
+            this.limPer = limPer;
+            this.calle = calle;
+            this.direccion = direccion;
+            this.fecha = fecha;
+        }
+        public Actividad()
+        {
+            this.idActiv = 0;
+            this.nombre = "";
+            this.descripcion = "";
+            this.edadMin = 0;
+            this.edadMax = 0;
+            this.limPer = 0;
+            this.calle = "";
+            this.direccion = 0;
+            this.fecha = DateTime.Now;
+        }
+    }
+}
